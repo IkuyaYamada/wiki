ede2b8599b0dc324089d034ea4428ed50d101e9c
Ikuya Yamada <dalyson.roman345@gmail.com>
2020-05-14 23:43:47 +0900

    Make a begining of Wiki Project

---
+++ dokuwiki/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,separated
+
+
+ikuyama:$1$JFcnY2Xc$MzOny/MzddbOMFB8M2VwY.:Ikuya Yamada:dalyson.roman345@gmail.com:admin,user
